e5ed469bd7f7041b7e185b288afec7c975713691
ma futain <a15453606@outlook.com>
2023-05-04 17:41:13 +0800

    抽象工厂

---
+++ 0202-Abstract-Factory/Entity.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0202_Abstract_Factory
+{
+    public class Button
+    {
+
+    }
+
+    public class WinButton: Button
+    {
+
+    }
+
+    public class MacButton: Button
+    {
+
+    }
+
+    public class CheckBox
+    {
+
+    }
+
+    public class WinCheckBox: CheckBox
+    {
+
+    }
+
+    public class MacCheckBox: CheckBox
+    {
+
+    }
+
+    public interface GUIFactory
+    {
+        public Button CreateButton();
+
+        public CheckBox CreateCheckBox();
+    }
+
+    public class DefaultGUIFactory: GUIFactory
+    {
+        public Button CreateButton()
+        {
+            return new Button();
+        }
+
+        public CheckBox CreateCheckBox()
+        {
+            return new CheckBox();
+        }
+    }
+
+    public class WinGUIFactory:GUIFactory
+    {
+        public Button CreateButton()
+        {
+            return new WinButton();
+        }
+
+        public CheckBox CreateCheckBox()
+        {
+            return new WinCheckBox();
+        }
+    }
+
+    public class MacGUIFactory : GUIFactory
+    {
+        public Button CreateButton()
+        {
+            return new MacButton();
+        }
+
+        public CheckBox CreateCheckBox()
+        {
+            return new MacCheckBox();
+        }
+    }
+}
---
+++ 0202-Abstract-Factory/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace _0202_Abstract_Factory
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var d = Console.ReadLine();
+
+            GUIFactory factory1 = new DefaultGUIFactory();
+            // 需要写Switch切换新加入的工厂
+            if (d.ToLower() == "windows")
+            {
+                factory1 = new WinGUIFactory();
+            }
+            else if (d.ToLower() == "mac")
+            {
+                factory1 = new MacGUIFactory();
+            }
+
+            new Application(factory1);
+
+        }
+
+        public class Application {
+
+            public Application(GUIFactory factory)
+            {
+                factory.CreateButton();
+                factory.CreateCheckBox();
+            }
+        }
+    }
+}
