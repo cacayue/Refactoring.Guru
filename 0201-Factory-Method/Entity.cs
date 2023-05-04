809584abb56b099deddf7fbf9b06152225913479
ma futain <a15453606@outlook.com>
2023-05-04 16:53:29 +0800

    工厂模式示例

---
+++ 0201-Factory-Method/Entity.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0201_Factory_Method
+{
+    public class Dialog
+    {
+        public virtual void Render()
+        {
+            var btn = CreateButton();
+            btn.Render();
+            btn.OnClick();
+            Console.WriteLine("Dialog Render");
+        }
+
+        protected virtual Button CreateButton()
+        {
+            return new Button();
+        }
+    }
+
+    public class WindowsDialog : Dialog
+    {
+        protected override Button CreateButton()
+        {
+            return new WindwosButton();
+        }
+    }
+    public class WebDialog : Dialog
+    {
+        protected override Button CreateButton()
+        {
+            return new HtmlButton();
+        }
+    }
+
+    public class Button
+    {
+        public virtual void Render()
+        {
+            Console.WriteLine("Button Render");
+        }
+
+        public virtual void OnClick()
+        {
+            Console.WriteLine("Button Click");
+        }
+
+    }
+
+    public class WindwosButton:Button
+    {
+        public override void OnClick()
+        {
+            Console.WriteLine("WindwosButton Click");
+        }
+
+    }
+
+    public class HtmlButton : Button
+    {
+        public override void OnClick()
+        {
+            Console.WriteLine("HtmlButton Click");
+        }
+
+    }
+}
---
+++ 0201-Factory-Method/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _0201_Factory_Method
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var d = Console.ReadLine();
+
+
+
+            var dialog = new Dialog();
+
+            if (d.ToLower() == "windows")
+            {
+                dialog = new WindowsDialog();
+            }
+            else if(d.ToLower() == "web")
+            {
+                dialog = new WebDialog();
+            }
+
+            dialog.Render();
+
+        }
+    }
+}
