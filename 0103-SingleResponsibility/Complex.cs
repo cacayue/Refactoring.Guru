f77adac4f4229812c77921ec7a33d997f671034b
ma futain <a15453606@outlook.com>
2023-05-04 15:38:33 +0800

    更新名称和增加单一职责原则示例

---
+++ 0103-SingleResponsibility/Complex.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0103_SingleResponsibility
+{
+    /// <summary>
+    /// 包含多个职能
+    /// </summary>
+    public class Employee
+    {
+        public string Name { get; set; }
+
+        public string GetName()
+        {
+            return Name;
+        }
+
+        public void PrintTimeSheetReport()
+        {
+            Console.WriteLine("111");
+        }
+    }
+}
---
+++ 0103-SingleResponsibility/Single.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0103_SingleResponsibility_Single
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+
+        public string GetName()
+        {
+            return Name;
+        }
+
+    }
+
+    public class TimeSheetReport
+    {
+        public void PrintTimeSheetReport()
+        {
+            Console.WriteLine("111");
+        }
+    }
+}
