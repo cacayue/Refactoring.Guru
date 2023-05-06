a1334567be610b418b7020692a07f3a279c013f0
ma futain <a15453606@outlook.com>
2023-05-06 11:27:42 +0800

    生成器模式

---
+++ 0203-Builder/Creator.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0203_Builder
+{
+    #region Entity
+
+    public class Car
+    {
+        public int Seats { get; set; }
+
+        public string Engine { get; set; } = "";
+
+        public bool GPS { get; set; }
+        public bool TripComputer { get; set; }
+
+        public override string ToString()
+        {
+            return $"this car is {this.Engine},Seats {Seats}, GPS：{this.GPS},TripComputer:{TripComputer} ";
+        }
+
+    }
+
+    public class CarManual
+    {
+        public int Seats { get; set; }
+
+        public string Engine { get; set; } = "";
+
+        public bool GPS { get; set; }
+
+        public bool TripComputer { get; set; }
+
+        public override string ToString()
+        {
+            return $"this manual is {this.Engine},Seats {Seats}, GPS：{this.GPS},TripComputer:{TripComputer} ";
+        }
+    }
+
+    #endregion
+
+    #region Builder
+
+    public interface Builder
+    {
+        public Builder Reset();
+
+        public Builder SetSeats(int number);
+
+        public Builder SetEngine(string engine);
+
+        public Builder SetTripComputer();
+
+        public Builder SetGPS();
+
+    }
+
+
+    public class CarBuilder : Builder
+    {
+        private Car Car { get; set; } = new Car();
+
+        public Builder Reset()
+        {
+            Car = new Car();
+            return this;
+        }
+
+        public Builder SetEngine(string engine)
+        {
+            Car.Engine = engine;
+            return this;
+        }
+
+        public Builder SetGPS()
+        {
+            Car.GPS = true;
+            return this;
+        }
+
+        public Builder SetSeats(int number)
+        {
+            Car.Seats = number;
+            return this;
+        }
+
+        public Builder SetTripComputer()
+        {
+            Car.TripComputer = true;
+            return this;
+        }
+
+        public Car GetResult()
+        {
+            return Car;
+        }
+    }
+
+    public class CarManualBuilder : Builder
+    {
+        private CarManual CarManual { get; set; } = new CarManual();
+
+        public Builder Reset()
+        {
+            CarManual = new CarManual();
+            return this;
+        }
+
+        public Builder SetEngine(string engine)
+        {
+            CarManual.Engine = engine;
+            return this;
+        }
+
+        public Builder SetGPS()
+        {
+            CarManual.GPS = true;
+            return this;
+        }
+
+        public Builder SetSeats(int number)
+        {
+            CarManual.Seats = number;
+            return this;
+        }
+
+        public Builder SetTripComputer()
+        {
+            CarManual.TripComputer = true;
+            return this;
+        }
+
+        public CarManual GetResult()
+        {
+            return CarManual;
+        }
+    }
+
+    #endregion
+
+    #region Director
+
+    public class Director
+    {
+        public void MakeSUV(Builder builder)
+        {
+            builder.Reset()
+                .SetSeats(2)
+                .SetEngine("SUV")
+                .SetGPS();
+        }
+
+        public void MakeSportsCar(Builder builder)
+        {
+            builder.Reset()
+                .SetSeats(3)
+                .SetEngine("SportsCar")
+                .SetTripComputer();
+        }
+    }
+
+    #endregion
+}
---
+++ 0203-Builder/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace _0203_Builder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var dirctor = new Director();
+            var carBuilder = new CarBuilder();
+            var carManualBuilder = new CarManualBuilder();
+            #region SUV
+
+            dirctor.MakeSUV(carBuilder);
+            dirctor.MakeSUV(carManualBuilder);
+            var suv = carBuilder.GetResult();
+            var suvManual = carManualBuilder.GetResult();
+
+            Console.Write(suv.ToString());
+            Console.WriteLine(suvManual.ToString());
+            #endregion
+
+            #region Sports
+
+            dirctor.MakeSportsCar(carBuilder);
+            dirctor.MakeSportsCar(carManualBuilder);
+            var sports = carBuilder.GetResult();
+            var sportsManual = carManualBuilder.GetResult();
+
+            Console.Write(sports.ToString());
+            Console.WriteLine(sportsManual.ToString());
+
+            #endregion
+
+
+        }
+    }
+}
