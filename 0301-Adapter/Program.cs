ec9781e5ac7d761498c63563ba37e1f4ac80d6dc
ma futain <a15453606@outlook.com>
2023-05-11 19:49:17 +0800

    适配器

---
+++ 0301-Adapter/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _0301_Adapter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            var roundPeg = new RoundPeg(5);
+            var roundHole = new RoundHole(5);
+
+
+            var ssquare = new SquarePeg(5);
+            var bsquare = new SquarePeg(10);
+
+
+            var rg = roundHole.Fits(roundPeg);
+            Console.WriteLine($"{rg}");
+
+            var sadapter = new SquarePegAdapter(ssquare);
+            var badapter = new SquarePegAdapter(bsquare);
+
+
+            var fg1 = roundHole.Fits(sadapter);
+            var fg2 = roundHole.Fits(badapter);
+            Console.WriteLine($"{fg1}");
+            Console.WriteLine($"{fg2}");
+
+        }
+    }
+}
---
+++ 0301-Adapter/Round.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0301_Adapter
+{
+    public class RoundPeg
+    {
+        public int Radius { get; set; }
+
+        public RoundPeg()
+        {
+
+        }
+
+        public RoundPeg(int radius)
+        {
+            Radius = radius;    
+        }
+
+        public virtual int GetRadius()
+        {
+            return Radius;
+        }
+    }
+
+    public class RoundHole
+    {
+        public int Radius { get; set; }
+
+        public RoundHole(int radius)
+        {
+            Radius = radius;
+        }
+
+        public int GetRadius()
+        {
+            return Radius; 
+        }
+
+        public bool Fits(RoundPeg peg)
+        {
+            return peg.GetRadius() <= Radius;
+        }
+    }
+
+    public class SquarePeg
+    {
+        public int Width { get; set; }
+
+        public SquarePeg(int width)
+        {
+            Width = width;
+        }
+
+        public int GetWidth()
+        {
+            return Width;
+        }
+    }
+
+    public class SquarePegAdapter: RoundPeg
+    {
+        public SquarePeg SquarePeg { get; set; }
+
+        public SquarePegAdapter(SquarePeg peg)
+        {
+            SquarePeg = peg;
+        }
+
+        public override int GetRadius()
+        {
+            var r = SquarePeg.GetWidth() * Math.Sqrt(2) / 2;
+            return (int)(r);
+        }
+    }
+}
