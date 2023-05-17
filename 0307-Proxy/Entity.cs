33374205c85376086fca10f0a7a410208528a2f1
ma futain <a15453606@outlook.com>
2023-05-17 15:03:40 +0800

    代理模式

---
+++ 0307-Proxy/Entity.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0307_Proxy
+{
+    public interface ThirdPartyTVLib
+    {
+        List<string> ListVideos();
+
+        string GetVideoInfo(string videoId);
+
+        string DownloadVideo(string videoId);
+    }
+
+    public class ThirdPartyTVClass: ThirdPartyTVLib
+    {
+        private List<string> _videos { get; set; }
+
+        public ThirdPartyTVClass()
+        {
+            _videos = new List<string> { "1", "2", "3" };
+        }
+
+        public List<string> ListVideos()
+        {
+            return _videos;
+        }
+
+        public string GetVideoInfo(string videoId)
+        {
+            return _videos.FirstOrDefault(x => x == videoId);
+        }
+
+        public string DownloadVideo(string videoId)
+        {
+            return _videos.FirstOrDefault(x => x == videoId);
+        }
+    }
+
+    public class CachedTVClass: ThirdPartyTVLib
+    {
+        public List<string> VideoCaches { get; set; }
+
+        public ThirdPartyTVLib Service { get; set; }
+
+        public CachedTVClass(ThirdPartyTVClass thirdParty)
+        {
+            Service = thirdParty;
+            VideoCaches = Service.ListVideos();
+        }
+
+        public string DownloadVideo(string videoId)
+        {
+            var exist = VideoCaches.FirstOrDefault(x => x == videoId);
+
+            if (exist == null)
+            {
+                exist = Service.DownloadVideo(videoId);
+                VideoCaches.Add(exist);
+            }
+
+            return exist;
+        }
+
+        public string GetVideoInfo(string videoId)
+        {
+            var exist = VideoCaches.FirstOrDefault(x => x == videoId);
+
+            if (exist == null)
+            {
+                exist = Service.GetVideoInfo(videoId);
+                VideoCaches.Add(exist);
+            }
+
+            return exist;
+        }
+
+        public List<string> ListVideos()
+        {
+            return VideoCaches;
+        }
+    }
+}
---
+++ 0307-Proxy/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _0307_Proxy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var tvService = new ThirdPartyTVClass();
+            var proxy = new CachedTVClass(tvService);
+
+            var list = proxy.ListVideos();
+
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
