0554ac0e1c33709bc53cba07d1ba82ff8ecd74eb
ma futain <a15453606@outlook.com>
2023-05-21 12:13:55 +0800

    命令模式补充

--- 0402-Command/Command.cs
+++ 0402-Command/Command.cs
@@ -8,48 +8,195 @@ namespace _0402_Command
 {
     public class Editor
     {
-        public string Text { get; set; }
+        private string _text { get; set; }
 
-        public void GetSelection()
+        public Editor(string text)
         {
+            _text = text;
+        }
 
+        public string GetSelection()
+        {
+            return _text;
         }
 
         public void DeleteSelection()
         {
-
+            _text = "";
         }
 
         public void ReplaceSelection(string text)
         {
-            Text = text;
+            _text = text;
         }
     }
 
     public abstract class Command
     {
-        public string App { get; set; }
+        public Application App { get; set; }
 
         public Editor Editor { get; set; }
 
         public string BackUp { get; set; }
 
-        public Command(string app, Editor editor)
+        public Command(Application app, Editor editor)
         {
             App = app;
             Editor = editor;
+            BackUp = "";
         }
 
-        public void SaveBackup()
+        protected virtual void SaveBackup()
+        {
+            BackUp = Editor.GetSelection();
+        }
+
+        protected virtual void Undo()
+        {
+            Editor.ReplaceSelection(BackUp);
+        }
+
+        public abstract bool Execut();
+    }
+
+    #region Commands
+
+    public class CopyCommand : Command
+    {
+        public Editor? Temp { get; set; }
+
+        public CopyCommand(Application app, Editor editor) : base(app, editor)
         {
 
+        }
+
+        public override bool Execut()
+        {
+            SaveBackup();
+
+            App.Clipboard = Editor.GetSelection();
+
+            return false;
+        }
+    }
+
+    public class CutCommand : Command
+    {
+
+        public CutCommand(Application app, Editor editor) : base(app, editor)
+        {
+
+        }
+
+        public override bool Execut()
+        {
+            SaveBackup();
+
+            App.Clipboard = Editor.GetSelection();
+
+            Editor.DeleteSelection();
+
+            return true;
+        }
+    }
+
+    public class PasteCommand : Command
+    {
+        public Editor? Temp { get; set; }
+
+        public PasteCommand(Application app, Editor editor) : base(app, editor)
+        {
+
+        }
+
+        public override bool Execut()
+        {
+            SaveBackup();
+
+            Editor.ReplaceSelection(App.Clipboard);
+
+            return true;
+        }
+    }
+
+    public class UndoCommand : Command
+    {
+        public CommandHistory CommandHistory { get; set; }
+
+        public UndoCommand(Application app, Editor editor,CommandHistory commandHistory) : base(app, editor)
+        {
+            CommandHistory = commandHistory;
+        }
+
+        public override bool Execut()
+        {
+            var oldCommand = CommandHistory.Pop();
+
+            if (oldCommand != null)
+            {
+                this.BackUp = oldCommand.BackUp;
+            }
+
+            Undo();
+
+            return false;
+        }
+    }
+
+    #endregion
+
+    public class CommandHistory
+    {
+        private Queue<Command> Commands { get; set; } = new Queue<Command>();
+
+        public void Push(Command command)
+        {
+            Commands.Enqueue(command);
+        }
+
+        public Command Pop()
+        {
+            return Commands.Dequeue();
+        }
+    }
+
+    public class Application
+    {
+        public List<Editor> Editors { get; set; }
+
+        public Editor ActiveEditor { get; set; }
+
+        public string Clipboard { get; set; }
+
+        public CommandHistory History { get; set; }
+
+        public Application()
+        {
+            Editors = new List<Editor>();
+            History = new CommandHistory();
+            Clipboard = "";
+        }
+
+        public void ExecuteCommand(Command command)
+        {
+            CacheEditor(command.Editor);
+            var res = command.Execut();
+            if (res)
+            {
+                History.Push(command);
+            }
         }
 
         public void Undo()
         {
-
+            var undo = new UndoCommand(this, ActiveEditor, History);
+            ExecuteCommand(undo);
         }
 
-        public abstract void execut();
+        private void CacheEditor(Editor editor)
+        {
+            Editors.Add(editor);
+            ActiveEditor = editor;
+        }
     }
 }
--- 0402-Command/Program.cs
+++ 0402-Command/Program.cs
@@ -4,7 +4,41 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var editor01 = new Editor("Text-----001");
+            var editor02 = new Editor("Text-----002");
+            var app = new Application();
+
+
+            var copyCommand = new CopyCommand(app, editor01);
+            var pasteCommand1 = new PasteCommand(app, editor01);
+            var cutCommand = new CutCommand(app, editor02);
+            var pasteCommand2 = new PasteCommand(app, editor02);
+         
+            
+
+            app.ExecuteCommand(copyCommand);
+            Console.WriteLine($"editor01 -- {editor01.GetSelection()}");
+            Console.WriteLine($"editor02 -- {editor02.GetSelection()}");
+
+            app.ExecuteCommand(pasteCommand2);
+            Console.WriteLine($"editor01 -- {editor01.GetSelection()}");
+            Console.WriteLine($"editor02 -- {editor02.GetSelection()}");
+
+            app.Undo();
+            Console.WriteLine($"editor01 -- {editor01.GetSelection()}");
+            Console.WriteLine($"editor02 -- {editor02.GetSelection()}");
+
+            app.ExecuteCommand(cutCommand);
+            Console.WriteLine($"editor01 -- {editor01.GetSelection()}");
+            Console.WriteLine($"editor02 -- {editor02.GetSelection()}");
+
+            app.Undo();
+            Console.WriteLine($"editor01 -- {editor01.GetSelection()}");
+            Console.WriteLine($"editor02 -- {editor02.GetSelection()}");
+
+            app.ExecuteCommand(pasteCommand1);
+            Console.WriteLine($"editor01 -- {editor01.GetSelection()}");
+            Console.WriteLine($"editor02 -- {editor02.GetSelection()}");
         }
     }
 }
